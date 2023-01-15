f559b322b430be5bd7ad995aa55a74e0ccad6546
Vitaz-Vitaz <76260006+Vitaz-Vitaz@users.noreply.github.com>
2023-01-15 15:25:26 +0300

    10

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import pygame
 pygame.init()
 WIDTH = 800
 HEIGHT = 640
+scrolling = 200
 ROWS = 16
+screen_scr = 0
+bgScr = 0
 COLS = 150
 all_images = list()
 for i in range(21):
@@ -16,6 +19,8 @@ for i in range(21):
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 FPS = 60
+back = pygame.image.load('imges/background/back.png').convert_alpha()
+sky = pygame.image.load('imges/background/sky.png').convert_alpha()
 pygame.display.set_caption("Stalker")
 GRAVITY = 2
 font = pygame.font.SysFont('Futura', 40)
@@ -51,7 +56,12 @@ class SpriteGroup(pygame.sprite.Group):
 
 def forFon():
     screen.fill((0, 0, 0))
-    pygame.draw.line(screen, (255, 255, 255), (0, 300), (WIDTH, 300))
+    w = back.get_width()
+    back2 = pygame.transform.scale(
+        back, (back.get_width(),
+               back.get_height()))
+    for i in range(5):
+        screen.blit(back2, (w * i - bgScr, 0))
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -157,9 +167,70 @@ class Player(Sprite):
             self.now_am -= 1
 
     def move(self):
-
+        global screen_scr
+        screen_scr = 0
         couldGoRight = True
         couldGoLeft = True
+        dx = 0
+        nY = 0
+        if self.jump and not self.isJump:
+            self.cy = -20
+            self.jump = False
+            self.isJump = True
+        self.cy += GRAVITY
+        if self.cy > 10:
+            self.cy = 0
+        nY += self.cy
+
+        for block in world.world:
+            if block[1].colliderect(self.rect.x, self.rect.y + nY, self.width, self.height):
+                if self.cy >= 0:
+                    self.cy = 0
+                    nY = block[1].top - self.rect.bottom
+                    self.isJump = False
+                elif self.cy < 0:
+                    self.cy = 0
+                    nY = block[1].bottom - self.rect.top
+
+        for block in world.world:
+            if block[1].colliderect(self.rect.x + self.skorost, self.rect.y, self.width, self.height):
+                couldGoRight = False
+                dx = 0
+                break
+            else:
+                couldGoRight = True
+                dx = 0
+
+        for block in world.world:
+            if block[1].colliderect(self.rect.x - self.skorost, self.rect.y, self.width, self.height):
+                couldGoLeft = False
+                break
+            else:
+                couldGoLeft = True
+
+        if self.go_left and couldGoLeft and self.rect.left - self.skorost > 0:
+            self.rect.x -= self.skorost
+            dx = -self.skorost
+            self.menyem = True
+            self.vlevoVpravo = -1
+        if self.go_right and couldGoRight and self.rect.right + self.skorost < WIDTH:
+            self.rect.x += self.skorost
+            self.menyem = True
+            dx = self.skorost
+            self.vlevoVpravo = 1
+        self.rect.y += nY
+
+        print(bgScr)
+        if bgScr < world.len * 40 - WIDTH:
+            if self.rect.right > WIDTH - scrolling:
+                self.rect.x -= self.skorost
+                screen_scr = -self.skorost
+            if self.rect.left < scrolling and bgScr > abs(dx):
+                self.rect.x += self.skorost
+                screen_scr = self.skorost
+        return screen_scr
+
+    def moveForEnemy(self):
 
         nY = 0
         if self.jump and not self.isJump:
@@ -181,34 +252,7 @@ class Player(Sprite):
                     self.cy = 0
                     nY = block[1].bottom - self.rect.top
 
-        for block in world.world:
-            if block[1].colliderect(self.rect.x + self.skorost, self.rect.y, self.width, self.height):
-                couldGoRight = False
-                break
-            else:
-                couldGoRight = True
-
-        for block in world.world:
-            if block[1].colliderect(self.rect.x - self.skorost, self.rect.y, self.width, self.height):
-                couldGoLeft = False
-                break
-            else:
-                couldGoLeft = True
-        if self.go_left and couldGoLeft:
-            self.rect.x -= self.skorost
-            self.menyem = True
-            self.vlevoVpravo = -1
-        if self.go_right and couldGoRight:
-            self.rect.x += self.skorost
-            self.menyem = True
-            self.vlevoVpravo = 1
-
         self.rect.y += nY
-
-    def moveForEnemy(self):
-        self.rect.y += GRAVITY
-        if self.rect.bottom > 300:
-            self.rect.bottom = 300
 
 
 class Bar():
@@ -232,6 +276,9 @@ class WaterBlock(Sprite):
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + 20, y + (40 - self.image.get_height()))
 
+    def update(self):
+        self.rect.x += screen_scr
+
 
 class ExitBlock(Sprite):
     def __init__(self, img, x, y):
@@ -239,6 +286,9 @@ class ExitBlock(Sprite):
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + 20, y + (40 - self.image.get_height()))
+
+    def update(self):
+        self.rect.x += screen_scr
 
 
 class Decor(Sprite):
@@ -248,23 +298,29 @@ class Decor(Sprite):
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + 20, y + (40 - self.image.get_height()))
 
+    def update(self):
+        self.rect.x += screen_scr
+
 
 class Box(Sprite):
     def __init__(self, x, y, type):
         pygame.sprite.Sprite.__init__(self)
+
         self.type = type
         self.image = pygame.image.load('imges/icons/' + type + '_box.png').convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
     def update(self):
+        self.rect.x += screen_scr
         if pygame.sprite.collide_rect(self, stalker):
             if self.type == 'health':
                 if stalker.health < 5:
                     stalker.health += 1
             elif self.type == 'ammo':
-                if stalker.am < 20:
-                    stalker.am = 20
+                if stalker.now_am < 20:
+                    stalker.now_am = 20
+                print("more ammo")
             self.kill()
 
 
@@ -279,9 +335,10 @@ class AllWorld():
         self.world = list()
 
     def load_world(self, d):
+        self.len = len(all_world[0])
         for y, ro in enumerate(d):
             for x, block in enumerate(ro):
-                print(block, len(all_images))
+
                 if block >= 0:
                     im = all_images[block]
                     imRect = im.get_rect()
@@ -293,6 +350,7 @@ class AllWorld():
                     elif 9 <= block <= 10:
                         water = WaterBlock(im, x * 40, y * 40)
                         water_group.add(water)
+
                     elif 11 <= block <= 14:
 
                         dec = Decor(im, x * 40, y * 40)
@@ -303,7 +361,7 @@ class AllWorld():
                     elif block == 16:
                         enemy = Player(x * 40, y * 40, 1.65, 15, "stalker", 0, 20)
                     elif block == 17:
-                        box1 = Box(x * 40, y * 40, 'ammo')
+                        box1 = Box(x * 40, y * 40 + 24, 'ammo')
                         box_group.add(box1)
                     elif block == 19:
                         box1 = Box(x * 40, y * 40, 'health')
@@ -315,6 +373,7 @@ class AllWorld():
 
     def draw(self):
         for block in self.world:
+            block[1][0] += screen_scr
             screen.blit(block[0], block[1])
 
 
@@ -332,9 +391,12 @@ class Bullet(Sprite):
         self.vlevovpavo = napravlenie
 
     def update(self):
-        self.rect.x += (self.vlevovpavo * self.speed)
-        if self.rect.right < 0 or self.rect.left > WIDTH - 100:
+        self.rect.x += (self.vlevovpavo * self.speed) + screen_scr
+        if self.rect.right < 0 or self.rect.left > WIDTH - 10:
             self.kill()
+        for block in world.world:
+            if block[1].colliderect(self.rect):
+                self.kill()
         if pygame.sprite.spritecollide(stalker, puly_group, False):
             if stalker.live:
                 stalker.health -= 1
@@ -354,6 +416,8 @@ enemy = Player(150, 150, 1.65, 15, "stalker", 0, 20)
 running = True
 
 while running:
+    water_group.draw(screen)
+    water_group.update()
 
     enemy.drawPlayer()
     enemy.update()
@@ -362,11 +426,10 @@ while running:
     box_group.update()
     box_group.draw(screen)
     decor_group.draw(screen)
+    decor_group.update()
     exit_group.draw(screen)
     exit_group.update()
-    water_group.draw(screen)
-    water_group.update()
-    decor_group.update()
+
     stalker.drawPlayer()
     enemy.moveForEnemy()
     drawText('Количество пуль: ', (255, 255, 255), 10, 10)
@@ -384,8 +447,9 @@ while running:
             stalker.proverka(1)
         else:
             stalker.proverka(0)
-        stalker.move()
 
+        screen_scr = stalker.move()
+        bgScr -= screen_scr
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             running = False
@@ -415,6 +479,7 @@ while running:
     puly_group.update()
     puly_group.draw(screen)
     clock.tick(FPS)
+
     forFon()
 
 pygame.quit()
